056eb3a79131c3c8ca6b6df1558c39c529170ac1
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-09-16 13:13:01 -0400

    Finished Algo Homework

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,7 @@
 const palindrome = str => {
-
+  let x = str.split(' ').join('')
+  let reverseX = x.split('').reverse().join('');
+  return reverseX === x;
 }
 
 console.log(palindrome("a man a plan a canal panama"))
